d5ffe7c707ea31b7fc7865e41b27dd025875f409
Edy Segura <edysegura@gmail.com>
2020-04-21 21:43:22 -0300

    Implement TableService.js

--- js/TableService.js
+++ js/TableService.js
@@ -1,11 +1,37 @@
 export class TableService {
 
-  static clearTable(table) {
+  static addRow(table) {
+    this.clearTable(table)
+    const [ tbody ] = table.tBodies
+    const row = tbody.insertRow(tbody.rows.length)
+    return row
+  }
 
+  static addCell(table) {
+    const row = this.addRow(table)
+    return (cellData) => {
+      row.insertCell(row.cells.length).innerHTML = !/true/.test(cellData)
+        ? cellData
+        : `<span>${cellData}</span>`
+    }
+  }
+
+  static clearTable(table) {
+    table.removeChild(table.tBodies[0])
+    table.appendChild(document.createElement('tbody'))
   }
 
   static buildTable(tableId, event) {
     const table = document.getElementById(tableId)
-  }
 
+    const cellsData = [
+      event.keyCode,
+      event.key,
+      event.ctrlKey,
+      event.altKey,
+      event.shiftKey
+    ]
+
+    cellsData.forEach(this.addCell(table))
+  }
 }
